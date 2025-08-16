23aeaf6e259c9cf3eae6f1afff51dc9475487c7b
Erik van Heijst <erik.van.heijst@sparrowquantum.com>
2025-08-16 17:56:44 +0200

    Style changes

--- lib/PyDotSim_Model.py
+++ lib/PyDotSim_Model.py
@@ -6,51 +6,54 @@ by Christian Heyn
 
 """
 
-
 import numpy as np
 import math
 import lib.PyDotSim_Constants as cnst
 
-T =         4
-xAl =       0.34
+T = 4
+xAl = 0.34
 statesNum = 3
 WFflat_threshold = 0.02
 
-MultiParticleLoops  = 1
+MultiParticleLoops = 1
 
 # colMap = 'gnuplot2'
 # colMap = 'jet'
-colMap = 'gray'
+colMap = "gray"
 
-QDshapeStr =        ['Ellipsoid', 'Disk','Cone','Cone-shell']
-QDshapeParaStr =    ['rx,  ry,  rz', 'r,  h,  --', 'r,  h,  --', 'r,  hH,  hQ']
-QDpara =            [1, 5e-9, 5e-9, 5e-9]           # QDpara:   [QD shape, r1, r2, r3]
-cellNum1D =         61
-cellSize =          (2*QDpara[1] + 6e-9)/cellNum1D
-cellVol =           cellSize*cellSize*cellSize
-meQ, mhQ =          cnst.AlGaAs_effMass(0)
-meB, mhB =          cnst.AlGaAs_effMass(xAl)
+QDshapeStr = ["Ellpisoid", "Disk", "Cone", "Cone-shell"]
+QDshapeParaStr = ["rx,  ry,  rz", "r,  h,  --", "r,  h,  --", "r,  hH,  hQ"]
+QDpara = [1, 5e-9, 5e-9, 5e-9]  # QDpara:   [QD shape, r1, r2, r3]
+cellNum1D = 61
+cellSize = (2 * QDpara[1] + 6e-9) / cellNum1D
+cellVol = cellSize * cellSize * cellSize
+meQ, mhQ = cnst.AlGaAs_effMass(0)
+meB, mhB = cnst.AlGaAs_effMass(xAl)
 smoothMaterialBorders = False
-EFields =           [0, 0]                          # EFields [Fz, Fx]
-BZfield =           [0, 0, 0]                       # BFields [Bz, le, lh]
-PointCharges =      []                              # Point charges
+EFields = [0, 0]  # EFields [Fz, Fx]
+BZfield = [0, 0, 0]  # BFields [Bz, le, lh]
+PointCharges = []  # Point charges
 
-MultiParticleStr =  ['X', 'XX', 'X+', 'X-']
-MPpara =            [0, 0]                          # MPpara:   [Particle, self-consistent loops]
+MultiParticleStr = ["X", "XX", "X+", "X-"]
+MPpara = [0, 0]  # MPpara:   [Particle, self-consistent loops]
 
-inputStr =      [ [ 'Size [nm]', 'r1', 'r2', 'r3' ],
-                  [ 'Cells num (1D)', 'N'],
-                  [ 'Cell size [nm]', '' ],
-                  [ 'E fields [V/m]', 'Fz', 'Fx' ],
-                  [ 'Bz field', 'Bz[T]', 'le' , 'lh']    ]
+inputStr = [
+    ["Size [nm]", "r1", "r2", "r3"],
+    ["Cells num (1D)", "N"],
+    ["Cell size [nm]", ""],
+    ["E fields [V/m]", "Fz", "Fx"],
+    ["Bz field", "Bz[T]", "le", "lh"],
+]
 
-inputVals =     []
-seriesStr =     ['r1', 'r2', 'r3', 'Fz', 'Fx', 'Bz']
-modelCreated =  False
+inputVals = []
+seriesStr = ["r1", "r2", "r3", "Fz", "Fx", "Bz"]
+modelCreated = False
 
 material = []
-Ve = []; Vh = []
-masse = []; massh = []
+Ve = []
+Vh = []
+masse = []
+massh = []
 
 
 def calcMaterialPara():
@@ -60,11 +63,11 @@ def calcMaterialPara():
 
 
 def setInputVals():
-    inputVals[0].set(QDpara[1]*1e9)
-    inputVals[1].set(QDpara[2]*1e9)
-    inputVals[2].set(QDpara[3]*1e9)
+    inputVals[0].set(QDpara[1] * 1e9)
+    inputVals[1].set(QDpara[2] * 1e9)
+    inputVals[2].set(QDpara[3] * 1e9)
     inputVals[3].set(cellNum1D)
-    inputVals[4].set( '{0: >#6.4f}'.format(cellSize*1e9) )
+    inputVals[4].set("{0: >#6.4f}".format(cellSize * 1e9))
     inputVals[5].set(EFields[0])
     inputVals[6].set(EFields[1])
     inputVals[7].set(BZfield[0])
@@ -74,11 +77,11 @@ def setInputVals():
 
 def readInputVals():
     global QDpara, cellNum1D, cellSize, meQ, mhQ, meB, mhB, EFields
-    QDpara[1] =  float(inputVals[0].get())/1e9
-    QDpara[2] =  float(inputVals[1].get())/1e9
-    QDpara[3] =  float(inputVals[2].get())/1e9
-    cellNum1D =    int(inputVals[3].get())
-    cellSize  =  float(inputVals[4].get())/1e9
+    QDpara[1] = float(inputVals[0].get()) / 1e9
+    QDpara[2] = float(inputVals[1].get()) / 1e9
+    QDpara[3] = float(inputVals[2].get()) / 1e9
+    cellNum1D = int(inputVals[3].get())
+    cellSize = float(inputVals[4].get()) / 1e9
     EFields[0] = float(inputVals[5].get())
     EFields[1] = float(inputVals[6].get())
     BZfield[0] = float(inputVals[7].get())
@@ -86,22 +89,25 @@ def readInputVals():
     BZfield[2] = float(inputVals[9].get())
 
 
-
 # returns the xyz position of a mesh cell with index cellIndx
 def cellPos(cellIndx):
-    x0 = -cellSize*(cellNum1D-1)/2
-    y0 = -cellSize*(cellNum1D-1)/2
-    z0 = -cellSize*(cellNum1D-1)/2
-    cx = x0 + cellIndx[0]*cellSize
-    cy = y0 + cellIndx[1]*cellSize
-    cz = z0 + cellIndx[2]*cellSize
+    x0 = -cellSize * (cellNum1D - 1) / 2
+    y0 = -cellSize * (cellNum1D - 1) / 2
+    z0 = -cellSize * (cellNum1D - 1) / 2
+    cx = x0 + cellIndx[0] * cellSize
+    cy = y0 + cellIndx[1] * cellSize
+    cz = z0 + cellIndx[2] * cellSize
     return cx, cy, cz
 
 
-x = []; y = []; z =[]
+x = []
+y = []
+z = []
+
+
 def createXYZ():
     global x, y, z, cellSize
-    #cellSize =  (2*QDpara[1] + 6e-9)/cellNum[0]
+    # cellSize =  (2*QDpara[1] + 6e-9)/cellNum[0]
     for i in range(cellNum1D):
         cx, cy, cz = cellPos([i, 0, 0])
         x.append(cx)
@@ -139,29 +145,36 @@ def cellQD(QDpara, pos):
                 inQD = True
     # Disk, r1: r, r2: h
     if QDshape == 2:
-        rQD = QDpara[1]; hQD = QDpara[2]
-        h0 = hQD/2
+        rQD = QDpara[1]
+        hQD = QDpara[2]
+        h0 = hQD / 2
         if abs(cz) <= h0:
-            cr = np.sqrt(cx**2+cy**2)
-            if cr <= rQD: inQD = True
+            cr = np.sqrt(cx**2 + cy**2)
+            if cr <= rQD:
+                inQD = True
     # Cone, r1: r, r2: h
     if QDshape == 3:
-        rQD = QDpara[1]; hQD = QDpara[2]
-        h0 = hQD/2
+        rQD = QDpara[1]
+        hQD = QDpara[2]
+        h0 = hQD / 2
         if abs(cz) <= h0:
-            rmax = (cz+h0)*rQD/hQD
-            cr = np.sqrt(cx**2+cy**2)
-            if cr <= rmax: inQD = True
-     # Cone shell, r1: r, r2: hHole, r3: hQd
-    if QDshape == 4:
-        rQD = QDpara[1]; hHole = QDpara[2]; hQD = QDpara[3]
-        h0 = hHole/2
-        if abs(cz) <= h0:
-            rmax = (cz+h0)*rQD/hHole
-            cr = np.sqrt(cx**2+cy**2)
+            rmax = (cz + h0) * rQD / hQD
+            cr = np.sqrt(cx**2 + cy**2)
             if cr <= rmax:
-                zmax = hQD-h0 + (hHole-hQD)*cr/rQD
-                if cz <= zmax: inQD = True
+                inQD = True
+    # Cone shell, r1: r, r2: hHole, r3: hQd
+    if QDshape == 4:
+        rQD = QDpara[1]
+        hHole = QDpara[2]
+        hQD = QDpara[3]
+        h0 = hHole / 2
+        if abs(cz) <= h0:
+            rmax = (cz + h0) * rQD / hHole
+            cr = np.sqrt(cx**2 + cy**2)
+            if cr <= rmax:
+                zmax = hQD - h0 + (hHole - hQD) * cr / rQD
+                if cz <= zmax:
+                    inQD = True
     return inQD
 
 
@@ -169,7 +182,8 @@ def cellQD(QDpara, pos):
 def cellMaterial(cellIndx):
     cx, cy, cz = cellPos(cellIndx)
     cmaterial = 1
-    if cellQD(QDpara, [cx, cy, cz]): cmaterial = 0
+    if cellQD(QDpara, [cx, cy, cz]):
+        cmaterial = 0
     return cmaterial
 
 
@@ -180,35 +194,36 @@ def cellMaterial(cellIndx):
 # add fields
 def createModel():
     global cellVol, material, Ve, Vh, masse, massh, modelCreated
-    cellVol = cellSize*cellSize*cellSize
-    N = cellNum1D; N3 = N*N*N
+    cellVol = cellSize * cellSize * cellSize
+    N = cellNum1D
+    N3 = N * N * N
     createXYZ()
 
-    material = np.zeros(N3).reshape(N,N,N)
-    masse = np.zeros(N3).reshape(N,N,N)
-    massh = np.zeros(N3).reshape(N,N,N)
-    Ve = np.zeros(N3).reshape(N,N,N)
-    Vh = np.zeros(N3).reshape(N,N,N)
+    material = np.zeros(N3).reshape(N, N, N)
+    masse = np.zeros(N3).reshape(N, N, N)
+    massh = np.zeros(N3).reshape(N, N, N)
+    Ve = np.zeros(N3).reshape(N, N, N)
+    Vh = np.zeros(N3).reshape(N, N, N)
 
     dEc, dEv = cnst.bandgapDiscontinuity(T, xAl)
     for ix in range(N):
         for iy in range(N):
             for iz in range(N):
-                material[ix,iy,iz] = cellMaterial([ix,iy,iz])
+                material[ix, iy, iz] = cellMaterial([ix, iy, iz])
     if smoothMaterialBorders:
         mGradientAll = np.gradient(material)
-        mGradient = np.add(np.abs(mGradientAll[0]),np.abs(mGradientAll[1]))
-        mGradient = np.add(mGradient,np.abs(mGradientAll[2]))
+        mGradient = np.add(np.abs(mGradientAll[0]), np.abs(mGradientAll[1]))
+        mGradient = np.add(mGradient, np.abs(mGradientAll[2]))
     for ix in range(N):
         for iy in range(N):
             for iz in range(N):
                 if smoothMaterialBorders:
-                    if abs(mGradient[ix,iy,iz]) != 0:
-                        material[ix,iy,iz] = 0.5
-                masse[ix,iy,iz] = material[ix,iy,iz]*(meB-meQ)+meQ
-                massh[ix,iy,iz] = material[ix,iy,iz]*(mhB-mhQ)+mhQ
-                Ve[ix,iy,iz] = material[ix,iy,iz]*dEc
-                Vh[ix,iy,iz] = material[ix,iy,iz]*dEv
+                    if abs(mGradient[ix, iy, iz]) != 0:
+                        material[ix, iy, iz] = 0.5
+                masse[ix, iy, iz] = material[ix, iy, iz] * (meB - meQ) + meQ
+                massh[ix, iy, iz] = material[ix, iy, iz] * (mhB - mhQ) + mhQ
+                Ve[ix, iy, iz] = material[ix, iy, iz] * dEc
+                Vh[ix, iy, iz] = material[ix, iy, iz] * dEv
 
     addEfields()
     addPCs()
@@ -220,23 +235,23 @@ def createModel():
 # Fx: Electric field [V/m] along x-direction
 def addEfields():
     global Ve, Vh
-    q = 1           # q for V in [eV}
+    q = 1  # q for V in [eV}
     Fz = float(EFields[0])
     if abs(Fz) > 0:
         for ix in range(len(Ve[0])):
             for iy in range(len(Ve[1])):
                 for iz in range(len(Ve[2])):
-                    x,y,z = cellPos([ix,iy,iz])
-                    Ve[ix, iy, iz] = Ve[ix, iy, iz] + q*Fz*z
-                    Vh[ix, iy, iz] = Vh[ix, iy, iz] - q*Fz*z
+                    x, y, z = cellPos([ix, iy, iz])
+                    Ve[ix, iy, iz] = Ve[ix, iy, iz] + q * Fz * z
+                    Vh[ix, iy, iz] = Vh[ix, iy, iz] - q * Fz * z
     Fx = float(EFields[1])
     if abs(Fx) > 0:
         for ix in range(len(Ve[0])):
             for iy in range(len(Ve[1])):
                 for iz in range(len(Ve[2])):
-                    x,y,z = cellPos([ix,iy,iz])
-                    Ve[ix, iy, iz] = Ve[ix, iy, iz] + q*Fx*x
-                    Vh[ix, iy, iz] = Vh[ix, iy, iz] - q*Fx*x
+                    x, y, z = cellPos([ix, iy, iz])
+                    Ve[ix, iy, iz] = Ve[ix, iy, iz] + q * Fx * x
+                    Vh[ix, iy, iz] = Vh[ix, iy, iz] - q * Fx * x
 
 
 # Point charges
@@ -244,19 +259,26 @@ def addPCs():
     if len(PointCharges) > 0:
         q = cnst.e
         N = cellNum1D
-        #c = 4*cnst.epsilon_GaAs*math.pi
+        # c = 4*cnst.epsilon_GaAs*math.pi
         for iPC in range(len(PointCharges)):
             for ix in range(N):
                 for iy in range(N):
                     for iz in range(N):
-                        x,y,z = cellPos([ix,iy,iz])
+                        x, y, z = cellPos([ix, iy, iz])
                         xPC = PointCharges[iPC][0]
                         yPC = PointCharges[iPC][1]
                         zPC = PointCharges[iPC][2]
-                        d = np.sqrt( pow((x-xPC),2) + pow((y-yPC),2) + pow((z-zPC),2) )
-                        if d == 0: d = model.cellSize/4
-                        Ve[ix, iy, iz] = Ve[ix, iy, iz] + q / (4*cnst.epsilon_GaAs*math.pi*d)
-                        Vh[ix, iy, iz] = Vh[ix, iy, iz] - q / (4*cnst.epsilon_GaAs*math.pi*d)
+                        d = np.sqrt(
+                            pow((x - xPC), 2) + pow((y - yPC), 2) + pow((z - zPC), 2)
+                        )
+                        if d == 0:
+                            d = model.cellSize / 4
+                        Ve[ix, iy, iz] = Ve[ix, iy, iz] + q / (
+                            4 * cnst.epsilon_GaAs * math.pi * d
+                        )
+                        Vh[ix, iy, iz] = Vh[ix, iy, iz] - q / (
+                            4 * cnst.epsilon_GaAs * math.pi * d
+                        )
 
 
 # Bz: Magnetic field [T] along z-direction
@@ -264,23 +286,27 @@ def addBZfield():
     Bz = BZfield[0]
     Le = BZfield[1]
     Lh = BZfield[2]
-    if (abs(Bz)>0) or (abs(Le)>0) or (abs(Lh)>0):
+    if (abs(Bz) > 0) or (abs(Le) > 0) or (abs(Lh) > 0):
         q = cnst.e
-        c0_e = (1/q)* cnst.hbar**2*Le**2 / (2*cnst.me_GaAs)     # in [eV]
-        c0_h = (1/q)* cnst.hbar**2*Lh**2 / (2*cnst.mhh_GaAs)    # in [eV]
-        c1_e =  (1/q)* q*cnst.hbar*Bz*Le / (2*cnst.me_GaAs)     # in [eV]
-        c1_h = (1/q)* -q*cnst.hbar*Bz*Lh / (2*cnst.mhh_GaAs)    # in [eV]
-        c2_e =  (1/q)* (q*Bz)**2 / (8*cnst.me_GaAs)             # in [eV]
-        c2_h = (1/q)* (-q*Bz)**2 / (8*cnst.mhh_GaAs)            # in [eV]
+        c0_e = (1 / q) * cnst.hbar**2 * Le**2 / (2 * cnst.me_GaAs)  # in [eV]
+        c0_h = (1 / q) * cnst.hbar**2 * Lh**2 / (2 * cnst.mhh_GaAs)  # in [eV]
+        c1_e = (1 / q) * q * cnst.hbar * Bz * Le / (2 * cnst.me_GaAs)  # in [eV]
+        c1_h = (1 / q) * -q * cnst.hbar * Bz * Lh / (2 * cnst.mhh_GaAs)  # in [eV]
+        c2_e = (1 / q) * (q * Bz) ** 2 / (8 * cnst.me_GaAs)  # in [eV]
+        c2_h = (1 / q) * (-q * Bz) ** 2 / (8 * cnst.mhh_GaAs)  # in [eV]
         N = cellNum1D
         for ix in range(N):
             for iy in range(N):
                 for iz in range(N):
-                    x,y,z = cellPos([ix,iy,iz])
-                    rxy2 =  (x**2 + y**2)
+                    x, y, z = cellPos([ix, iy, iz])
+                    rxy2 = x**2 + y**2
                     if rxy2 > 0:
-                        Ve[ix, iy, iz] = Ve[ix, iy, iz] + c0_e/rxy2 + c1_e + c2_e*rxy2
-                        Vh[ix, iy, iz] = Vh[ix, iy, iz] + c0_h/rxy2 + c1_h + c2_h*rxy2
+                        Ve[ix, iy, iz] = (
+                            Ve[ix, iy, iz] + c0_e / rxy2 + c1_e + c2_e * rxy2
+                        )
+                        Vh[ix, iy, iz] = (
+                            Vh[ix, iy, iz] + c0_h / rxy2 + c1_h + c2_h * rxy2
+                        )
 
 
 # Dirichlet boundary condition in 1D: psi(0) = psi(N) = 0
@@ -288,21 +314,40 @@ def addBZfield():
 def createBoundaries(V):
     N = cellNum1D
     for ix in range(len(V[0])):
-            for iy in range(len(V[1])):
-                for iz in range(len(V[2])):
-                    if ix==0 or ix==N-1: V[ix, iy, iz] = 100
-                    if iy==0 or iy==N-1: V[ix, iy, iz] = 100
-                    if iz==0 or iz==N-1: V[ix, iy, iz] = 100
+        for iy in range(len(V[1])):
+            for iz in range(len(V[2])):
+                if ix == 0 or ix == N - 1:
+                    V[ix, iy, iz] = 100
+                if iy == 0 or iy == N - 1:
+                    V[ix, iy, iz] = 100
+                if iz == 0 or iz == N - 1:
+                    V[ix, iy, iz] = 100
     return V
 
 
 # shows planes through the calculated model potential
 def ShowPotential(figModel, canvasModel):
     figModel.clf()
-    ix=cellNum1D//2; iy=cellNum1D//2; iz=cellNum1D//2;
-    figModel.add_subplot(131).imshow(Ve[ix, : , : ], interpolation='none', cmap=colMap)
-    figModel.add_subplot(132).imshow(Ve[: , iy, : ], interpolation='none', cmap=colMap)
-    figModel.add_subplot(133).imshow(Ve[: , : , iz], interpolation='none', cmap=colMap)
+    ix = cellNum1D // 2
+    iy = cellNum1D // 2
+    iz = cellNum1D // 2
+
+    xy_plane = figModel.add_subplot(131)
+    xy_im = xy_plane.imshow(Ve[:, :, iz], interpolation="none", cmap=colMap)
+    figModel.colorbar(xy_im)
+    xy_plane.set_title("XY plane")
+
+    xz_plane = figModel.add_subplot(132)
+    xz_im = xz_plane.imshow(Ve[:, iy, :], interpolation="none", cmap=colMap)
+    figModel.colorbar(xz_im)
+    xz_plane.set_title("XZ plane")
+
+    yz_plane = figModel.add_subplot(133)
+    yz_im = yz_plane.imshow(Ve[ix, :, :], interpolation="none", cmap=colMap)
+    figModel.colorbar(yz_im)
+    yz_plane.set_title("YZ plane")
+
+    figModel.tight_layout()
     canvasModel.draw()
 
 
@@ -311,6 +356,5 @@ def autoCellSize():
     global cellSize
     borderSize = 5e-9
     readInputVals()
-    cellSize =  (2*QDpara[1] + 2*borderSize)/cellNum1D
+    cellSize = (2 * QDpara[1] + 2 * borderSize) / cellNum1D
     setInputVals()
-
--- lib/PyDotSim_Results.py
+++ lib/PyDotSim_Results.py
@@ -6,7 +6,6 @@ by Christian Heyn
 
 """
 
-
 import csv
 import matplotlib.lines as lines
 import lib.PyDotSim_Constants as cnst
@@ -16,19 +15,48 @@ import lib.PyDotSim_WF as WF
 
 # colMap = 'gnuplot2'
 # colMap = 'jet'
-colMap = 'gray'
+colMap = "inferno"
 
-resultsHeader =     ['shape', 'r1', 'r2', 'r3', 'cells', 'cellSize', 'MP', 'MP#',
-                     'meQ', 'mhQ', 'meB', 'mhB', 'Fz', 'Fx', 'Bz', 'Le', 'Lh', 'E0e', 'E0h',
-                     'rxe', 'rye', 'rze', 'rxh', 'ryh', 'rzh', 'deh',
-                     'overlap', 'tau', 'Ceh', ' Ex_single', 'Ep']
-resultsSingle =     []
-results =           []
+resultsHeader = [
+    "shape",
+    "r1",
+    "r2",
+    "r3",
+    "cells",
+    "cellSize",
+    "MP",
+    "MP#",
+    "meQ",
+    "mhQ",
+    "meB",
+    "mhB",
+    "Fz",
+    "Fx",
+    "Bz",
+    "Le",
+    "Lh",
+    "E0e",
+    "E0h",
+    "rxe",
+    "rye",
+    "rze",
+    "rxh",
+    "ryh",
+    "rzh",
+    "deh",
+    "overlap",
+    "tau",
+    "Ceh",
+    " Ex_single",
+    "Ep",
+]
+resultsSingle = []
+results = []
 
-Ee = [];    Eh =    []
-Ex_single =         0
-Ep =                0
-
+Ee = []
+Eh = []
+Ex_single = 0
+Ep = 0
 
 
 # Add QD shape to WF plot
@@ -37,51 +65,109 @@ def addQDShape(ax):
     QDshape = model.QDpara[0]
     # Ellipse, so far only sphere
     if QDshape == 1:
-        rxQD = 1e9*model.QDpara[1]; ryQD = 1e9*model.QDpara[2]; rzQD = 1e9*model.QDpara[3];
+        rxQD = 1e9 * model.QDpara[1]
+        ryQD = 1e9 * model.QDpara[2]
+        rzQD = 1e9 * model.QDpara[3]
         pass
     # Disk, r1: r, r2: h
     if QDshape == 2:
-        rQD = 1e9*model.QDpara[1]; hQD = 1e9*model.QDpara[2]
+        rQD = 1e9 * model.QDpara[1]
+        hQD = 1e9 * model.QDpara[2]
         pass
     # Cone, r1: r, r2: h
     if QDshape == 3:
-        rQD = 1e9*model.QDpara[1]; hQD = 1e9*model.QDpara[2]
+        rQD = 1e9 * model.QDpara[1]
+        hQD = 1e9 * model.QDpara[2]
         pass
-     # Cone shell, r1: r, r2: hHole, r3: hQD
+    # Cone shell, r1: r, r2: hHole, r3: hQD
     if QDshape == 4:
-        rQD = model.QDpara[1]/model.cellSize; hHole = model.QDpara[2]/model.cellSize; hQD = model.QDpara[3]/model.cellSize
-        ix0 = (model.cellNum1D-1)/2
-        iz0 = ix0-hHole/2
-        line = lines.Line2D([iz0, iz0+hHole], [ix0, ix0+rQD], lw=1, color='red')
+        rQD = model.QDpara[1] / model.cellSize
+        hHole = model.QDpara[2] / model.cellSize
+        hQD = model.QDpara[3] / model.cellSize
+        ix0 = (model.cellNum1D - 1) / 2
+        iz0 = ix0 - hHole / 2
+        line = lines.Line2D([iz0, iz0 + hHole], [ix0, ix0 + rQD], lw=1, color="red")
         ax.add_line(line)
-        line = lines.Line2D([iz0+hQD, iz0+hHole], [ix0, ix0+rQD], lw=1, color='red')
+        line = lines.Line2D(
+            [iz0 + hQD, iz0 + hHole], [ix0, ix0 + rQD], lw=1, color="red"
+        )
         ax.add_line(line)
-        line = lines.Line2D([iz0+hQD, iz0+hHole], [ix0, ix0-rQD], lw=1, color='red')
+        line = lines.Line2D(
+            [iz0 + hQD, iz0 + hHole], [ix0, ix0 - rQD], lw=1, color="red"
+        )
         ax.add_line(line)
-        line = lines.Line2D([iz0, iz0+hHole], [ix0, ix0-rQD], lw=1, color='red')
+        line = lines.Line2D([iz0, iz0 + hHole], [ix0, ix0 - rQD], lw=1, color="red")
         ax.add_line(line)
 
 
 # shows planes through the calculated wave functions
 def showPSI(figResults, canvasResults):
-    interpol = 'bicubic'
-    #interpol = 'none'
+    interpol = "bicubic"
+    # interpol = 'none'
     figResults.clf()
-    i=model.cellNum1D//2
-    psie = WF.psie; psih = WF.psih
+    i = model.cellNum1D // 2
+    psie = WF.psie
+    psih = WF.psih
     # probability density
-    psie = psie**2; psih = psih**2
-    figResults.add_subplot(231).imshow(psie[0][i , : , :], interpolation=interpol, cmap=colMap)
-    figResults.add_subplot(232).imshow(psie[0][: , i , :], interpolation=interpol, cmap=colMap)
-    figResults.add_subplot(233).imshow(psie[0][: , : , i], interpolation=interpol, cmap=colMap)
-    figResults.add_subplot(234).imshow(psih[0][i , : , :], interpolation=interpol, cmap=colMap)
-    figResults.add_subplot(235).imshow(psih[0][: , i , :], interpolation=interpol, cmap=colMap)
-    figResults.add_subplot(236).imshow(psih[0][: , : , i], interpolation=interpol, cmap=colMap)
+    psie = psie**2
+    psih = psih**2
+
+    psie_xy = figResults.add_subplot(231)
+    psie_xy_im = psie_xy.imshow(psie[0][:, :, i], interpolation=interpol, cmap=colMap)
+    figResults.colorbar(psie_xy_im)
+    psie_xy.set_title("WFe | XY")
+
+    psie_xz = figResults.add_subplot(232)
+    psie_xz_im = psie_xz.imshow(psie[0][:, i, :], interpolation=interpol, cmap=colMap)
+    figResults.colorbar(psie_xz_im)
+    psie_xz.set_title("WFe | XZ")
+
+    psie_yz = figResults.add_subplot(233)
+    psie_yz_im = psie_yz.imshow(psie[0][i, :, :], interpolation=interpol, cmap=colMap)
+    figResults.colorbar(psie_yz_im)
+    psie_yz.set_title("WFe | YZ")
+
+    psih_xy = figResults.add_subplot(234)
+    psih_xy_im = psih_xy.imshow(psih[0][:, :, i], interpolation=interpol, cmap=colMap)
+    figResults.colorbar(psih_xy_im)
+    psih_xy.set_title("WFh | XY")
+
+    psih_xz = figResults.add_subplot(235)
+    psih_xz_im = psih_xz.imshow(psih[0][:, i, :], interpolation=interpol, cmap=colMap)
+    figResults.colorbar(psih_xz_im)
+    psih_xz.set_title("WFh | XZ")
+
+    psih_yz = figResults.add_subplot(236)
+    psih_yz_im = psih_yz.imshow(psih[0][i, :, :], interpolation=interpol, cmap=colMap)
+    figResults.colorbar(psih_yz_im)
+    psih_yz.set_title("WFh | YZ")
+
+    # figResults.add_subplot(231).imshow(
+    #     psie[0][i, :, :], interpolation=interpol, cmap=colMap
+    # )
+    # figResults.add_subplot(232).imshow(
+    #     psie[0][:, i, :], interpolation=interpol, cmap=colMap
+    # )
+    # figResults.add_subplot(233).imshow(
+    #     psie[0][:, :, i], interpolation=interpol, cmap=colMap
+    # )
+    # figResults.add_subplot(234).imshow(
+    #     psih[0][i, :, :], interpolation=interpol, cmap=colMap
+    # )
+    # figResults.add_subplot(235).imshow(
+    #     psih[0][:, i, :], interpolation=interpol, cmap=colMap
+    # )
+    # figResults.add_subplot(236).imshow(
+    #     psih[0][:, :, i], interpolation=interpol, cmap=colMap
+    # )
     # add QD shape
-    addQDShape(figResults.axes[0])
-    addQDShape(figResults.axes[1])
-    addQDShape(figResults.axes[3])
-    addQDShape(figResults.axes[4])
+    # addQDShape(figResults.axes[0])
+    # addQDShape(figResults.axes[1])
+    # addQDShape(figResults.axes[3])
+    # addQDShape(figResults.axes[4])
+
+    figResults.tight_layout()
+
     canvasResults.draw()
 
 
@@ -90,48 +176,52 @@ def E0peak():
     T = 4
     Eg = cnst.GaAs_bandgap(T)
     Ex_single = Eg + abs(Ee[0]) + abs(Eh[0])
-    Ex =  Ex_single - WF.Ceh
+    Ex = Ex_single - WF.Ceh
     particle = model.MultiParticleStr[model.MPpara[0]]
-    if particle == 'X':     Ep = Ex
-    if particle == 'XX':    Ep = Ex - (2*WF.Ceh - WF.Cee - WF.Chh)
-    if particle == 'X+':    Ep = Ex - (WF.Ceh - WF.Chh)
-    if particle == 'X-':    Ep = Ex - (WF.Ceh - WF.Cee)
+    if particle == "X":
+        Ep = Ex
+    if particle == "XX":
+        Ep = Ex - (2 * WF.Ceh - WF.Cee - WF.Chh)
+    if particle == "X+":
+        Ep = Ex - (WF.Ceh - WF.Chh)
+    if particle == "X-":
+        Ep = Ex - (WF.Ceh - WF.Cee)
 
 
 def createResultsSingle():
     global resultsSingle, results
     resultsSingle = []
-    resultsSingle.append( model.QDshapeStr[model.QDpara[0]-1] )
-    resultsSingle.append( str(model.QDpara[1]*1e9) )
-    resultsSingle.append( str(model.QDpara[2]*1e9) )
-    resultsSingle.append( str(model.QDpara[3]*1e9) )
-    resultsSingle.append( str(model.cellNum1D) )
-    resultsSingle.append( str(model.cellSize*1e9) )
-    resultsSingle.append( model.MultiParticleStr[model.MPpara[0]] )
-    resultsSingle.append( str(model.MultiParticleLoops) )
-    resultsSingle.append( str(model.meQ) )
-    resultsSingle.append( str(model.mhQ) )
-    resultsSingle.append( str(model.meB) )
-    resultsSingle.append( str(model.mhB) )
-    resultsSingle.append( str(model.EFields[0]) )
-    resultsSingle.append( str(model.EFields[1]) )
-    resultsSingle.append( str(model.BZfield[0]) )
-    resultsSingle.append( str(model.BZfield[1]) )
-    resultsSingle.append( str(model.BZfield[2]) )
-    resultsSingle.append( str(Ee[0]) )
-    resultsSingle.append( str(Eh[0]) )
-    resultsSingle.append( str(WF.rxe*1e9) )
-    resultsSingle.append( str(WF.rye*1e9) )
-    resultsSingle.append( str(WF.rze*1e9) )
-    resultsSingle.append( str(WF.rxh*1e9) )
-    resultsSingle.append( str(WF.ryh*1e9) )
-    resultsSingle.append( str(WF.rzh*1e9) )
-    resultsSingle.append( str(WF.deh*1e9) )
-    resultsSingle.append( str(WF.overlap) )
-    resultsSingle.append( str(WF.tau) )
-    resultsSingle.append( str(WF.Ceh) )
-    resultsSingle.append( str(str(Ex_single)) )
-    resultsSingle.append( str(str(Ep)) )
+    resultsSingle.append(model.QDshapeStr[model.QDpara[0] - 1])
+    resultsSingle.append(str(model.QDpara[1] * 1e9))
+    resultsSingle.append(str(model.QDpara[2] * 1e9))
+    resultsSingle.append(str(model.QDpara[3] * 1e9))
+    resultsSingle.append(str(model.cellNum1D))
+    resultsSingle.append(str(model.cellSize * 1e9))
+    resultsSingle.append(model.MultiParticleStr[model.MPpara[0]])
+    resultsSingle.append(str(model.MultiParticleLoops))
+    resultsSingle.append(str(model.meQ))
+    resultsSingle.append(str(model.mhQ))
+    resultsSingle.append(str(model.meB))
+    resultsSingle.append(str(model.mhB))
+    resultsSingle.append(str(model.EFields[0]))
+    resultsSingle.append(str(model.EFields[1]))
+    resultsSingle.append(str(model.BZfield[0]))
+    resultsSingle.append(str(model.BZfield[1]))
+    resultsSingle.append(str(model.BZfield[2]))
+    resultsSingle.append(str(Ee[0]))
+    resultsSingle.append(str(Eh[0]))
+    resultsSingle.append(str(WF.rxe * 1e9))
+    resultsSingle.append(str(WF.rye * 1e9))
+    resultsSingle.append(str(WF.rze * 1e9))
+    resultsSingle.append(str(WF.rxh * 1e9))
+    resultsSingle.append(str(WF.ryh * 1e9))
+    resultsSingle.append(str(WF.rzh * 1e9))
+    resultsSingle.append(str(WF.deh * 1e9))
+    resultsSingle.append(str(WF.overlap))
+    resultsSingle.append(str(WF.tau))
+    resultsSingle.append(str(WF.Ceh))
+    resultsSingle.append(str(str(Ex_single)))
+    resultsSingle.append(str(str(Ep)))
     results.append(resultsSingle)
 
 
@@ -142,10 +232,7 @@ def initResults():
 
 
 def saveResults(FileName_data):
-    #FileName_data = os.getcwd()+'/'+name+', '+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'.dat'
-    with open(FileName_data, 'w', newline='') as f:
-        writer = csv.writer(f, delimiter='\t')
+    # FileName_data = os.getcwd()+'/'+name+', '+datetime.datetime.now().strftime("%Y%m%d-%H%M%S")+'.dat'
+    with open(FileName_data, "w", newline="") as f:
+        writer = csv.writer(f, delimiter="\t")
         writer.writerows(results)
-
-
-
