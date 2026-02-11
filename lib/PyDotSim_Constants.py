1b89bc65a06024607f4a7d7201f1845aef6361a0
Erik van Heijst <erik.van.heijst@sparrowquantum.com>
2026-02-11 23:01:20 +0100

    Added two versions of InGaAs + styling

--- lib/PyDotSim_Constants.py
+++ lib/PyDotSim_Constants.py
@@ -5,6 +5,7 @@ Constants for SimQDStates
 by Christian Heyn
 
 """
+
 # %%
 import math
 import sys
@@ -100,7 +101,7 @@ class Alloy:
                 0.024 + 0.035 * (1 - self.x) + 0.008 * (1 - self.x) ** 2
             )
             alloy_hole_mass = 0.53  # for 30% Indium
-        #else:
+        # else:
         #    raise ValueError("No such alloy name is recognized")
 
         return alloy_electron_mass, alloy_hole_mass
@@ -114,11 +115,11 @@ class Alloy:
             return dEc, dEv
         if self.name == "InGaAs":
             dE = self._bandgap() - self.host.band_gap
-            f = 60 / 40  # GUESS from Yu et al
+            f = 50 / 50  # GUESS from Yu et al
             dEc = dE * f / (1 + f)
             dEv = dE - dEc
             return dEc, dEv
-        #else:
+        # else:
         #    raise ValueError("No such alloy name is recognized")
 
 
@@ -157,11 +158,15 @@ indium_arsenide = Material(
     temperature=300,
 )
 
-#indium_arsenide.calculate_derived_properties()
-#gallium_arsenide.calculate_derived_properties()
+# indium_arsenide.calculate_derived_properties()
+# gallium_arsenide.calculate_derived_properties()
 
 indium_gallium_arsenide = Alloy(
     host=indium_arsenide, guest=gallium_arsenide, x=0.3, C=0.477, name="InGaAs"
+)
+
+indium_gallium_arsenide_qd = Alloy(
+    host=indium_arsenide, guest=gallium_arsenide, x=0.6, C=0.477, name="InGaAs"
 )
 
 aluminium_gallium_arsenide = Alloy(
@@ -174,58 +179,76 @@ aluminium_gallium_arsenide = Alloy(
 
 ####
 
-h = 6.62606876e-34;             # Planck's constant [J*s]
-hbar = h/(2*math.pi);           # Planck's constant reduced [J*s]
-epsilon0 = 8.854187817e-12;     # Vacuum permittivity [F/m] = [A*s/(V*m)]
-me0 = 9.10938188e-31;           # Electron mass [kg]
-c = 299792458;                  # speed of light in m/s
-e = 1.60217662e-19;             # elementary charge in coulombs
-JeV = 1/1.60217646e-19;         # Joule in eV
+h = 6.62606876e-34
+# Planck's constant [J*s]
+hbar = h / (2 * math.pi)
+# Planck's constant reduced [J*s]
+epsilon0 = 8.854187817e-12
+# Vacuum permittivity [F/m] = [A*s/(V*m)]
+me0 = 9.10938188e-31
+# Electron mass [kg]
+c = 299792458
+# speed of light in m/s
+e = 1.60217662e-19
+# elementary charge in coulombs
+JeV = 1 / 1.60217646e-19
+# Joule in eV
 
 # GaAs QDs related constants
 me = 0.067
-me_GaAs = me*me0                                        # GaAs effective electron mass [kg]
+me_GaAs = me * me0  # GaAs effective electron mass [kg]
 mh = 0.51
-mhh_GaAs = mh*me0                                       # GaAs effective heavy hole mass [kg]
-epsilon_GaAs = 13.1*epsilon0;                           # GaAs permittivity
-m_Ex = 1/(1/me_GaAs + 1/mhh_GaAs);                      # Exciton effective mass
-r_Ex_GaAs = 4*math.pi*epsilon_GaAs*hbar**2/(m_Ex*e**2); # GaAs exciton Bohr radius in [m]
-E_Ry_GaAs = m_Ex*e**4/(2*(2*epsilon_GaAs*h)**2);        # Rydberg constant in GaAs bulk in [J]
-E_p = 28.8;                                             # Kane energy in eV
-n = 3.6;                                                # refractive index
+mhh_GaAs = mh * me0  # GaAs effective heavy hole mass [kg]
+epsilon_GaAs = 13.1 * epsilon0
+# GaAs permittivity
+m_Ex = 1 / (1 / me_GaAs + 1 / mhh_GaAs)
+# Exciton effective mass
+r_Ex_GaAs = 4 * math.pi * epsilon_GaAs * hbar**2 / (m_Ex * e**2)
+# GaAs exciton Bohr radius in [m]
+E_Ry_GaAs = m_Ex * e**4 / (2 * (2 * epsilon_GaAs * h) ** 2)
+# Rydberg constant in GaAs bulk in [J]
+E_p = 28.8
+# Kane energy in eV
+n = 3.6
+# refractive index
 
-def GaAs_bandgap(T): # Varshni
+
+def GaAs_bandgap(T):  # Varshni
     # Vurgaftman, Thurmond
-    EGaAs0 = 1.519; a = 5.405e-4; b = 204;
-    EGaAs = EGaAs0 - a*T*T/(T+b);
-    #gallium_arsenide.calculate_derived_properties()
-    #EGaAs = indium_arsenide.band_gap
+    # EGaAs0 = 1.519; a = 5.405e-4; b = 204;
+    # EGaAs = EGaAs0 - a*T*T/(T+b);
+    # gallium_arsenide.calculate_derived_properties()
+    EGaAs = indium_gallium_arsenide_qd._bandgap()
     return EGaAs
 
-def AlAs_bandgap(T): # Varshni
+
+def AlAs_bandgap(T):  # Varshni
     # Vurgaftman
-    EGaAs0 = 3.099; a = 8.85e-4; b = 530;
-    EGaAs = EGaAs0 - a*T*T/(T+b);
-    #aluminium_arsenide.calculate_derived_properties()
-    #EGaAs = indium_arsenide.band_gap
+    # EGaAs0 = 3.099; a = 8.85e-4; b = 530;
+    # EGaAs = EGaAs0 - a*T*T/(T+b);
+    # aluminium_arsenide.calculate_derived_properties()
+    EGaAs = indium_arsenide.band_gap
     return EGaAs
 
-def AlGaAs_bandgap(T, x): # Varshni
-    # Vurgaftman
-    C = -0.127 + 1.310*x
-    EGaAs = GaAs_bandgap(T)
-    EAlAs = AlAs_bandgap(T)
-    EAlGaAs = EGaAs + (EAlAs-EGaAs-C)*x+C*x*x
 
-    #EAlGaAs = indium_gallium_arsenide._bandgap()
+def AlGaAs_bandgap(T, x):  # Varshni
+    # Vurgaftman
+    # C = -0.127 + 1.310*x
+    # EGaAs = GaAs_bandgap(T)
+    # EAlAs = AlAs_bandgap(T)
+    # EAlGaAs = EGaAs + (EAlAs-EGaAs-C)*x+C*x*x
+
+    EAlGaAs = indium_gallium_arsenide._bandgap()
     return EAlGaAs
 
-def AlGaAs_effMass(x):
-    me = 0.067+0.057*x
-    mhh = 0.51+0.25*x
 
-    #me, mhh = indium_gallium_arsenide._effective_mass()
-    return round(me,4), round(mhh,4)
+def AlGaAs_effMass(x):
+    # me = 0.067+0.057*x
+    # mhh = 0.51+0.25*x
+
+    me, mhh = indium_gallium_arsenide._effective_mass()
+    return round(me, 4), round(mhh, 4)
+
 
 # GaAs - AlGaAs valence band edge discontinuity
 # from Adachi, Properties of semiconductor alloys, 2009: dEc/dEv = 63/37 = f
@@ -234,16 +257,25 @@ def AlGaAs_effMass(x):
 # dEc+dE*f = dEc*(1+f) = dE*f
 # dEc = dE*f/(1+f)
 def bandgapDiscontinuity(T, x):
-    EGaAs = GaAs_bandgap(T)
-    EAlGaAs = AlGaAs_bandgap(T, x)
-    dE = EAlGaAs-EGaAs
-    f = 63/37
-    dEc = dE*f/(1+f)
-    dEv = dE-dEc
+    # EGaAs = GaAs_bandgap(T)
+    # EAlGaAs = AlGaAs_bandgap(T, x)
+    # dE = EAlGaAs-EGaAs
+    # f = 63/37
+    # dEc = dE*f/(1+f)
+    # dEv = dE-dEc
 
-    #dEc, dEv = indium_gallium_arsenide._bandgap_discontuity()
+    dEc, dEv = indium_gallium_arsenide._bandgap_discontuity()
     return dEc, dEv
 
+
 def QDlifetime(overlapp, E_PL):
-    tau = (1/2) * 3 * h**2 * c**3 * epsilon0 * me0 / (math.pi * n * e**2 * E_p/JeV * E_PL/JeV * overlapp)
+    tau = (
+        (1 / 2)
+        * 3
+        * h**2
+        * c**3
+        * epsilon0
+        * me0
+        / (math.pi * n * e**2 * E_p / JeV * E_PL / JeV * overlapp)
+    )
     return tau
